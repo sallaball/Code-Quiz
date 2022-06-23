44f8b22673e458566d38e2e4cdf5496def204e2b
sallaball <sallaball1206@gmail.com>
2022-06-23 09:14:07 -0500

    working on js, about to start on bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,4 +141,155 @@ function changeQuestion() {
     currentQues++;
 }
 
+function endOfQuiz() {
+    title.textContent = "All Done.";
+    timeLeft = 1;
+    clearOptions();
+    clearAnswer();
+    content.setAttribute("style", "display: visible");
+    content.textContent = `Your final score is ${score}`;
+    inputFields();
+}
+
+function clearOptions() {
+    for(let i = 0; i < optionList.length; i++) {
+        optionList[i].remove();
+    }
+    optionList = [];
+}
+
+function inputFields() {
+    let initialsForm = document.createElement("form");
+    container.appendChild(initialsForm);
+    initialsForm.setAttribute("id", "form");
+    let label = document.createElement("label");
+    initialsForm.appendChild(label);
+    label.textContent = "Enter initials: "
+    let input = document.createElement("input")
+    initialsForm.appendChild(input);
+    input.setAttribute("id", "initials");
+    let submit = document.createElement("button");
+    initialsForm.appendChild(submit);
+    submit.setAttribute("id", "submit");
+    submit.textContent = "Submit";
+
+    title.setAttribute("style", "align-self: start")
+    content.setAttribute("style", "align-self: start; font-size: 150%");
+
+    
+    input.addEventListener("keydown", stopReload);
+    submit.addEventListener("click", addScore);
+    
+}
+
+function stopReload(event) {
+    if(event.key === "Enter") {
+        event.preventDefault();
+    }
+}
+
+function addScore(event) {
+    if(event !== undefined) {
+        event.preventDefault();
+    }
+    let id = document.getElementById("initials");
+    if(id.value.length > 3 || id.value.length === 0) {
+        invalidInput();
+        return;
+    }
+    isQuizOngoing = false;
+    document.getElementById("form").remove();
+    saveScore(id);
+}
+
+function saveScore(id) {
+    if(localStorage.getItem("leaderboard") !== null) {
+        leaderboard = JSON.parse(localStorage.getItem("leaderboard"));
+    }
+    leaderboard.push(`${score} ${id.value}`);
+    localStorage.setItem("leaderboard", JSON.stringify(leaderboard));
+    showScores();    
+}
+
+function invalidInput() {
+    answer.textContent = "Initials must be entered and three characters or less";
+    answer.setAttribute("style", "color: black");
+    clearAnswer();
+    let submit = document.getElementById("submit");
+    submit.addEventListener("click", addScore);
+}
+
+function showScores() {
+    if(!isQuizOngoing) {
+        title.textContent = "High Scores";
+        // Hides start quiz button if view high scores is clicked at beginning
+        start.setAttribute("style", "display: none");
+        writeScores();
+        createEndButtons();
+    } else if(title.textContent === "All Done.") {
+        answer.textContent = "Please enter your initials first";
+        answer.setAttribute("style", "color: black");
+        clearAnswer();
+    } else {
+        answer.textContent = "Cannot view scores until the quiz is over";
+        answer.setAttribute("style", "color: black");
+        clearAnswer();
+    }
+}
+
+function writeScores() {
+    content.textContent = "";
+    content.setAttribute("style", "white-space: pre-wrap; font-size: 150%");
+    if(localStorage.getItem("leaderboard") !== null) {
+        leaderboard = JSON.parse(localStorage.getItem("leaderboard"));
+    }
+    leaderboard.sort();
+    leaderboard.reverse();
+    let limit = 11;
+    if(limit > leaderboard.length) {
+        limit = leaderboard.length;
+    }
+    for(let i = 0; i < limit; i++) {
+        content.textContent += leaderboard[i] + '\n';
+    }
+}
+
+function createEndButtons() {
+    if(!document.getElementById("restart")) {
+        let restartVar = document.createElement("button");
+        container.appendChild(restartVar);
+        restartVar.textContent = "Go Back";
+        restartVar.setAttribute("id", "restart");
+        
+        let clearScoresVar = document.createElement("button");
+        container.appendChild(clearScoresVar);
+        clearScoresVar.textContent = "Clear High Scores";
+        clearScoresVar.setAttribute("id", "clearScores");
+        
+        restartVar.addEventListener("click", restart);
+        clearScoresVar.addEventListener("click", clearScores)
+    }
+}
+
+function restart() {
+    title.setAttribute("style", "align-self: center");
+    content.setAttribute("style", "align-self: center; font-size: 110%");
+    document.getElementById("restart").remove();
+    document.getElementById("clearScores").remove();
+    title.textContent = "Coding Quiz Challenge";
+    content.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your time by reducing it by ten seconds.";
+    start.setAttribute("style", "display: visible");
+    currentQues = 0;
+    score = 0;
+    timeLeft = 61;
+    init();
+}
+
+function clearScores() {
+    localStorage.clear();
+    content.textContent = "";
+    leaderboard = [];
+}
+
+init();
 
