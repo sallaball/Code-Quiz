b6abe7ea85a33ff8c044481dd9bb81a1c11d6860
sallaball <sallaball1206@gmail.com>
2022-06-23 07:37:45 -0500

    with some help, js is now started

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//variables
+
+var scores = document.querySelector("#scores");
+var timer = document.querySelector("#timer");
+var intro = document.querySelector("#header-intro");
+var title = document.querySelector("#title");
+var content = document.querySelector("#content");
+var start = document.querySelector("#start-button");
+var questions = document.querySelector("#questions");
